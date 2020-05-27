630e6bfc07c19c6983c9fd89d0db7e0bc2a961f6
R-VdP <141248+R-VdP@users.noreply.github.com>
2020-05-27 22:14:23 +0200

    Several improvements.

--- src/Main.elm
+++ src/Main.elm
@@ -20,7 +20,8 @@ main = Browser.element { init = init
 
 type ModelState = Init
                 | Ready Config
-                | Restarted RestartedState
+                | Restarting RestartedState
+                | Done RestartedState (Maybe String)
 
 type alias Model = { state : ModelState
                    , log   : List String
@@ -35,6 +36,7 @@ type alias RestartedState = { total : Int
 type Msg = ConfigMsg (Result Http.Error Config)
          | RestartMsg Config
          | RestartDoneMsg (Result Http.Error String)
+         | FoundGifMsg (Result Http.Error String)
 
 printError : Http.Error -> String
 printError error = case error of
@@ -60,7 +62,9 @@ update : Msg -> Model -> (Model, Cmd Msg)
 update msg model = case msg of
   ConfigMsg result      -> (gotConfig model result, Cmd.none)
   RestartMsg cfg        -> (gotRestart model cfg, doRestart cfg)
-  RestartDoneMsg result -> (gotRestartDone model result, Cmd.none)
+  RestartDoneMsg result -> let newModel = gotRestartDone model result
+                           in (newModel, restartDoneCmd newModel)
+  FoundGifMsg result    -> (gotGif model result, Cmd.none)
 
 gotConfig : Model -> (Result Http.Error Config) -> Model
 gotConfig model res = case res of
@@ -69,19 +73,33 @@ gotConfig model res = case res of
   Err err -> appendLog model (printError err)
 
 gotRestart : Model -> Config -> Model
-gotRestart model cfg = appendLog { model | state = Restarted { total = List.length cfg.hosts, count = 0 } }
+gotRestart model cfg = appendLog { model | state = Restarting { total = List.length cfg.hosts, count = 0 } }
                                  ("Restarting servers...")
 
 doRestart : Config -> Cmd Msg
-doRestart cfg = Cmd.batch (List.map restartServer cfg.hosts)
+doRestart cfg = Cmd.batch << (List.map restartServer) <| cfg.hosts
 
 gotRestartDone : Model -> (Result Http.Error String) -> Model
 gotRestartDone model res = case res of
   Ok  host_status -> case model.state of
-    Restarted state -> appendLog { model | state = Restarted { state | count = state.count + 1 } }
-                                 host_status
-    _               -> appendLog model "Model in unexpected state"
-  Err err  -> appendLog model (printError err)
+    Restarting state -> appendLog { model | state = Restarting { state | count = state.count + 1 } }
+                                  host_status
+    _                -> appendLog model "Model in unexpected state, ignoring..."
+  Err err  -> appendLog model << printError <| err
+
+restartDoneCmd : Model -> Cmd Msg
+restartDoneCmd model = case model.state of
+  Restarting state -> if state.count == state.total then getRandomCatGif else Cmd.none
+  _                -> Cmd.none
+
+gotGif : Model -> (Result Http.Error String) -> Model
+gotGif model res = case model.state of
+  Restarting state ->
+    let newModel = case res of
+                     Ok  url -> { model | state = Done state (Maybe.Just url) }
+                     Err err -> appendLog { model | state = Done state Maybe.Nothing} << printError <| err
+    in appendLog newModel "Reached final state."
+  _                -> appendLog model "Model in unexpected state, ignoring..."
 
 subscriptions : Model -> Sub Msg
 subscriptions model = Sub.none
@@ -102,9 +120,10 @@ viewElement model =
      , printLog model
      ]
      ( case model.state of
-         Init         -> el [Font.size 30] ( text "Loading the app config..." )
-         Ready cfg    -> viewReady model cfg
-         Restarted st -> viewRestarted model st
+         Init          -> el [Font.size 30] ( text "Loading the app config..." )
+         Ready cfg     -> viewReady model cfg
+         Restarting st -> viewRestarted model st Maybe.Nothing
+         Done st url   -> viewRestarted model st url
      )
 
 viewReady : Model -> Config -> Element Msg
@@ -120,8 +139,8 @@ viewReady model cfg =
               ( button "Restart the servers" button_url (RestartMsg cfg) )
          ]
 
-viewRestarted : Model -> RestartedState -> Element Msg
-viewRestarted model state =
+viewRestarted : Model -> RestartedState -> Maybe String -> Element Msg
+viewRestarted model state maybe_url =
   column [ Element.width fill
          , Element.height fill
          ]
@@ -130,8 +149,12 @@ viewRestarted model state =
                   ]
                   [ text ("Restarting " ++ (String.fromInt state.total) ++ " servers.")
                   , text ("Progress: " ++ (String.fromInt state.count) ++ "/" ++ (String.fromInt state.total))
+                  , Maybe.withDefault Element.none << Maybe.map renderImg <| maybe_url
                   ]
          ]
+
+renderImg : String -> Element Msg
+renderImg url = image [] { src = url, description = "Funny cat, we're done!" }
 
 printLog : Model -> Element.Attribute Msg
 printLog model =
@@ -140,7 +163,7 @@ printLog model =
                             ]
                             ( column []
                                      [ text "Debug log:\n"
-                                     , text (String.join "\n" model.log)
+                                     , text << String.join "\n" <| model.log
                                      ]
                             )
                         )
@@ -168,7 +191,7 @@ getConfig = Http.get { url = config_url
                      }
 
 configDecoder : Decoder Config
-configDecoder = J.map Config (J.field "servers" (J.list J.string))
+configDecoder = J.map Config << J.field "servers" << J.list <| J.string
 
 restartServer : String -> Cmd Msg
 restartServer host = Http.get { url = restart_url ++ "?host=" ++ host --"https://" ++ host ++ "/api/restart"
@@ -177,7 +200,15 @@ restartServer host = Http.get { url = restart_url ++ "?host=" ++ host --"https:/
                               }
 
 restartedDecoder : String -> Decoder String
-restartedDecoder host = J.map (\status -> host ++ ": " ++ status) (J.field "status" J.string)
+restartedDecoder host = J.map (\status -> host ++ ": " ++ status) << J.field "status" <| J.string
+
+getRandomCatGif : Cmd Msg
+getRandomCatGif = Http.get { url = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cat"
+                           , expect = Http.expectJson FoundGifMsg gifDecoder
+                           }
+
+gifDecoder : Decoder String
+gifDecoder = J.field "data" << J.field "image_url" <| J.string
 
 config_url: String
 config_url = "https://gist.githubusercontent.com/R-VdP/4947fc2c79918c2ae02398a326c3bc63/raw/fc4a2cc81f50e426275b7dff87c26d724439720d/config"
