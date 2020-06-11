35df2563f6ef741bfe58977d99522f75e7050cea
R-VdP <141248+R-VdP@users.noreply.github.com>
2020-06-11 23:41:43 +0200

    Simplify the decorator.

--- backend/msfocb/panic_button.py
+++ backend/msfocb/panic_button.py
@@ -12,19 +12,28 @@ from gevent.pywsgi import WSGIServer
 # Decorator to verify the key in a request
 # See https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
 #
-# To use, annotate the endpoint with @key_required and introduce a boolean
-# named argument "key_valid" to receive the result
-def key_required(wrapped):
-  @wraps(wrapped)
-  def validate_key(*args, **kwargs):
-    request_key = request.args.get('key')
-    time_input = int(time.time()) // 2
-    m = hashlib.sha256()
-    m.update(bytes(str(time_input), 'utf-8'))
-    calculated_key = m.hexdigest()
-    key_valid = (request_key == calculated_key)
-    return wrapped(*args, **kwargs, key_valid = key_valid)
-  return validate_key
+# To use, annotate the endpoint with @key_required and pass the Response
+# to return if the key is invalid.
+# Be careful to pass the response as a function object to be called from
+# within the decorator and not to call it while defining the decorator,
+# otherwise the request context will not be available when the function
+# is called.
+def key_required(invalid_response: Response):
+  def decorator(wrapped):
+    @wraps(wrapped)
+    def validate_key(*args, **kwargs):
+      request_key = request.args.get('key')
+      time_input = int(time.time()) // 2
+      m = hashlib.sha256()
+      m.update(bytes(str(time_input), 'utf-8'))
+      calculated_key = m.hexdigest()
+      key_valid = (request_key == calculated_key)
+      if request_key == calculated_key:
+        return wrapped(*args, **kwargs)
+      else:
+        return invalid_response()
+    return validate_key
+  return decorator
 
 static = "static"
 app = Flask(__name__,
@@ -44,6 +53,15 @@ def args_parser():
 
 args = args_parser().parse_args()
 
+def return_status(status: bool) -> Response:
+  return jsonify({ 'status': ('OK' if status else 'NOK') })
+
+def return_ok() -> Response:
+  return return_status(True)
+
+def return_nok() -> Response:
+  return return_status(False)
+
 @app.route('/')
 def root() -> Response:
   return send_from_directory(static, 'index.html')
@@ -56,30 +74,21 @@ def config() -> Response:
                  })
 
 @app.route('/api/lock', methods=['POST'])
-@key_required
-def lock(key_valid: bool) -> Response:
-  if key_valid:
-    if request.args.get('mock') == 'true':
-      print("Mock is set to true, ignoring...")
-      return return_status(True)
-    else:
-      print("Mock is set to false, locking...")
-      p = subprocess.run(args.lock_script.split())
-      return return_status(p.returncode == 0)
+@key_required(return_nok)
+def lock() -> Response:
+  if request.args.get('mock') == 'true':
+    print("Mock is set to true, ignoring...")
+    return return_status(True)
   else:
-    return return_status(False)
+    print("Mock is set to false, locking...")
+    p = subprocess.run(args.lock_script.split())
+    return return_status(p.returncode == 0)
 
 @app.route('/api/verify', methods=['GET'])
-@key_required
-def verify(key_valid: bool) -> Response:
-  if key_valid:
-    p = subprocess.run(args.verify_script.split())
-    return return_status(p.returncode == 0)
-  else:
-    return return_status(False)
-
-def return_status(status: bool) -> Response:
-  return jsonify({ 'status': ('OK' if status else 'NOK') })
+@key_required(return_nok)
+def verify() -> Response:
+  p = subprocess.run(args.verify_script.split())
+  return return_status(p.returncode == 0)
 
 def main():
   http_server = WSGIServer(('', 1234), app)
