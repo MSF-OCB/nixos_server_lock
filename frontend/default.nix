69349ce86c924a33d416572447e37daadd984a12
R-VdP <141248+R-VdP@users.noreply.github.com>
2020-06-12 13:19:04 +0200

    Refactor frontend build to properly separate the build and install phases.

--- frontend/default.nix
+++ frontend/default.nix
@@ -4,6 +4,7 @@ with nixpkgs;
 with lib;
 
 let
+  dist = "dist";
   mkDerivation =
     { srcs ? ./elm-srcs.nix
     , src
@@ -18,32 +19,38 @@ let
 
       buildInputs = [ elmPackages.elm nodePackages_10_x.uglify-js ];
 
-      buildPhase = pkgs.elmPackages.fetchElmDeps {
+      preBuildPhases = [ "setupElmStuffPhase" ];
+
+      setupElmStuffPhase = pkgs.elmPackages.fetchElmDeps {
         elmPackages = import srcs;
-        elmVersion = "0.19.1";
+        elmVersion  = "0.19.1";
         inherit registryDat;
       };
 
-      installPhase = let
+      buildPhase = let
         elmfile = module: "${srcdir}/${builtins.replaceStrings ["."] ["/"] module}.elm";
-        extension = "js";
-        build_module = module: ''
+        build_module = out: module: let
+          out_file = "${out}/generated/${module}.js";
+        in ''
           echo "compiling ${elmfile module}"
-          elm make --optimize ${elmfile module} --output $out/generated/${module}.${extension}
+          elm make --optimize ${elmfile module} --output ${out_file}
           ${optionalString production ''
-            echo "minifying ${module}.${extension}"
-            uglifyjs $out/generated/${module}.${extension} \
+            echo "minifying ${out_file}"
+            uglifyjs ${out_file} \
                      --compress \
                      'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe,passes=2' \
-              | uglifyjs --mangle --output=$out/generated/${module}.${extension}
+              | uglifyjs --mangle --output=${out_file}
           ''}
         '';
       in ''
-        mkdir -p $out/assets
-        ${concatMapStrings build_module targets}
+        mkdir -p ${dist}/
+        ${concatMapStrings (build_module dist) targets}
+      '';
 
+      installPhase = ''
+        mkdir -p $out
         echo "copying assets"
-        cp -r index.html assets $out
+        cp -r index.html assets ${dist}/generated $out
       '';
     };
 in mkDerivation {
