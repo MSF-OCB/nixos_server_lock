19af8a3111a619d842bf6b15ad06685da468bba8
R-VdP <141248+R-VdP@users.noreply.github.com>
2020-06-17 19:12:02 +0200

    Properly configure the logging framework.

--- backend/msfocb/panic_button.py
+++ backend/msfocb/panic_button.py
@@ -8,6 +8,23 @@ from flask_compress import Compress
 from flask_cors import cross_origin
 from functools import wraps
 from gevent.pywsgi import WSGIServer
+from logging.config import dictConfig
+
+dictConfig({
+  'version': 1,
+  'formatters': {'default': {
+    'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+  }},
+  'handlers': {'wsgi': {
+    'class': 'logging.StreamHandler',
+    'stream': 'ext://flask.logging.wsgi_errors_stream',
+    'formatter': 'default'
+  }},
+  'root': {
+    'level': 'INFO',
+    'handlers': ['wsgi']
+  }
+})
 
 # Decorator to verify the key in a request
 # See https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
