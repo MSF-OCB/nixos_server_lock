ce10d7b7ae6e5354d1701155a5ec2f7f78587679
Sohel Sarder <113350522+sardersohel@users.noreply.github.com>
2024-09-04 16:13:56 +0200

    rename python file name (#14)
    * rename python file name
    
    * More refactor in content and source files
    
    * More refactor in content and source files

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -70,7 +70,7 @@ main =
         { init = init
         , update = update
         , subscriptions = subscriptions
-        , view = \model -> { title = "Panic Button", body = [ view model ] }
+        , view = \model -> { title = "NixOS Server Lock Button", body = [ view model ] }
         }
 
 
@@ -605,7 +605,7 @@ viewElement model =
                 , Font.center
                 ]
                 [ nonBreakingText [ "MSF", "server " ]
-                , el [ Font.color red ] <| nonBreakingText [ "panic", "button" ]
+                , el [ Font.color red ] <| nonBreakingText [ "nixos server lock", "button" ]
                 ]
 
         mainElement =
